8cef27d9e31319114dcf28f86b021c860377bf2c
jobiols <jorge.obiols@gmail.com>
2019-09-27 23:19:01 -0300

    [FIX] modules

--- nato_default/__manifest__.py
+++ nato_default/__manifest__.py
@@ -49,21 +49,14 @@
     # Here begins docker-odoo-environment manifest
     # --------------------------------------------
 
-    # if Enterprise it installs in a different directory than community
-    'Enterprise': False,
-
     # port where odoo starts serving pages
     'port': '8069',
-
-    # syntax version of repos and images
-    'env_ver': 1,
-
-    # example repos version 1
     'repos': [
         {'usr': 'jobiols', 'repo': 'cl-nato', 'branch': '11.0'},
+        {'usr': 'jobiols', 'repo': 'odoo-com', 'branch': '11.0',
+         'host': 'bitbucket.org'},
+
         {'usr': 'jobiols', 'repo': 'odoo-addons', 'branch': '11.0'},
-        {'usr': 'jobiols', 'repo': 'rafi16jan-backend-theme',
-         'branch': '11.0'},
 
         {'usr': 'jobiols', 'repo': 'adhoc-odoo-argentina', 'branch': '11.0'},
         {'usr': 'jobiols', 'repo': 'adhoc-argentina-sale', 'branch': '11.0'},
@@ -85,27 +78,10 @@
         {'usr': 'jobiols', 'repo': 'oca-server-brand', 'branch': '11.0'},
     ],
 
-    # example images version 1
     'docker': [
         {'name': 'odoo', 'usr': 'jobiols', 'img': 'odoo-jeo', 'ver': '11.0'},
         {'name': 'postgres', 'usr': 'postgres', 'ver': '11.1-alpine'},
         {'name': 'nginx', 'usr': 'nginx', 'ver': 'latest'},
         {'name': 'aeroo', 'usr': 'adhoc', 'img': 'aeroo-docs'},
     ],
-
-    # example repos version 2
-    # Note that the branch of the repo to download is taken from the module
-    # version
-    'git-repos': [
-        'https://github.com/jobiols/cl-nato.git',
-        'https://github.com/jobiols/odoo-addons.git',
-        'https://github.com/jobiols/adhoc-odoo-argentina.git',
-    ],
-
-    # example images version 2
-    'docker-images': [
-        {'img': 'jobiols/odoo-jeo:11.0', 'name': 'odoo'},
-        {'img': 'postgres:11.1-alpine', 'name': 'postgres'},
-        {'img': 'adhoc/aeroo', 'name': 'aeroo'},
-    ]
 }
